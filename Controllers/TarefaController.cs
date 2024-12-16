da52948c86f911b36d51d016aea0e9de0e32cc32
Yuri Rocha <yurirocha15nascimento@outlook.com>
2024-12-16 12:30:47 -0300

    ObterPorId feito

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -21,7 +21,14 @@ namespace TrilhaApiDesafio.Controllers
             // TODO: Buscar o Id no banco utilizando o EF
             // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
             // caso contrário retornar OK com a tarefa encontrada
-            return Ok();
+            //FEITO
+
+            var tarefa = _context.Tarefas.Find(id);
+
+            if (tarefa == null)
+                return NotFound();
+
+            return Ok(tarefa);
         }
 
         [HttpGet("ObterTodos")]
